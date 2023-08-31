9dc91d7eb758f0b8d4a1ad438539294a33ccd691
alexefrain17 <1432461@senati.pe>
2023-08-31 17:11:19 -0500

    interfas de la calculadora con datos

---
+++ elpepe/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace elpepe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tbDisplay = new TextBox();
+            num7 = new Button();
+            num4 = new Button();
+            num1 = new Button();
+            num0 = new Button();
+            num5 = new Button();
+            num2 = new Button();
+            igual = new Button();
+            num9 = new Button();
+            num6 = new Button();
+            num3 = new Button();
+            mas = new Button();
+            menos = new Button();
+            multi = new Button();
+            divi = new Button();
+            punto = new Button();
+            num8 = new Button();
+            SuspendLayout();
+            // 
+            // tbDisplay
+            // 
+            tbDisplay.Location = new Point(12, 12);
+            tbDisplay.Name = "tbDisplay";
+            tbDisplay.Size = new Size(330, 23);
+            tbDisplay.TabIndex = 0;
+            // 
+            // num7
+            // 
+            num7.Location = new Point(12, 74);
+            num7.Name = "num7";
+            num7.Size = new Size(75, 23);
+            num7.TabIndex = 1;
+            num7.Text = "7";
+            num7.UseVisualStyleBackColor = true;
+            num7.Click += num7_Click;
+            // 
+            // num4
+            // 
+            num4.Location = new Point(12, 145);
+            num4.Name = "num4";
+            num4.Size = new Size(75, 23);
+            num4.TabIndex = 2;
+            num4.Text = "4";
+            num4.UseVisualStyleBackColor = true;
+            num4.Click += num4_Click;
+            // 
+            // num1
+            // 
+            num1.Location = new Point(12, 210);
+            num1.Name = "num1";
+            num1.Size = new Size(75, 23);
+            num1.TabIndex = 3;
+            num1.Text = "1";
+            num1.UseVisualStyleBackColor = true;
+            num1.Click += num1_Click;
+            // 
+            // num0
+            // 
+            num0.Location = new Point(12, 274);
+            num0.Name = "num0";
+            num0.Size = new Size(75, 23);
+            num0.TabIndex = 4;
+            num0.Text = "0";
+            num0.UseVisualStyleBackColor = true;
+            num0.Click += num0_Click;
+            // 
+            // num5
+            // 
+            num5.Location = new Point(93, 145);
+            num5.Name = "num5";
+            num5.Size = new Size(75, 23);
+            num5.TabIndex = 6;
+            num5.Text = "5";
+            num5.UseVisualStyleBackColor = true;
+            num5.Click += num5_Click;
+            // 
+            // num2
+            // 
+            num2.Location = new Point(93, 210);
+            num2.Name = "num2";
+            num2.Size = new Size(75, 23);
+            num2.TabIndex = 7;
+            num2.Text = "2";
+            num2.UseVisualStyleBackColor = true;
+            num2.Click += num2_Click;
+            // 
+            // igual
+            // 
+            igual.Location = new Point(140, 274);
+            igual.Name = "igual";
+            igual.Size = new Size(109, 23);
+            igual.TabIndex = 8;
+            igual.Text = "=";
+            igual.UseVisualStyleBackColor = true;
+            // 
+            // num9
+            // 
+            num9.Location = new Point(174, 74);
+            num9.Name = "num9";
+            num9.Size = new Size(75, 23);
+            num9.TabIndex = 9;
+            num9.Text = "9";
+            num9.UseVisualStyleBackColor = true;
+            num9.Click += num9_Click;
+            // 
+            // num6
+            // 
+            num6.Location = new Point(174, 145);
+            num6.Name = "num6";
+            num6.Size = new Size(75, 23);
+            num6.TabIndex = 10;
+            num6.Text = "6";
+            num6.UseVisualStyleBackColor = true;
+            num6.Click += num6_Click;
+            // 
+            // num3
+            // 
+            num3.Location = new Point(174, 210);
+            num3.Name = "num3";
+            num3.Size = new Size(75, 23);
+            num3.TabIndex = 11;
+            num3.Text = "3";
+            num3.UseVisualStyleBackColor = true;
+            num3.Click += num3_Click;
+            // 
+            // mas
+            // 
+            mas.Location = new Point(255, 74);
+            mas.Name = "mas";
+            mas.Size = new Size(75, 23);
+            mas.TabIndex = 12;
+            mas.Text = "+";
+            mas.UseVisualStyleBackColor = true;
+            // 
+            // menos
+            // 
+            menos.Location = new Point(255, 145);
+            menos.Name = "menos";
+            menos.Size = new Size(75, 23);
+            menos.TabIndex = 13;
+            menos.Text = "-";
+            menos.UseVisualStyleBackColor = true;
+            // 
+            // multi
+            // 
+            multi.Location = new Point(255, 210);
+            multi.Name = "multi";
+            multi.Size = new Size(75, 23);
+            multi.TabIndex = 14;
+            multi.Text = "*";
+            multi.UseVisualStyleBackColor = true;
+            // 
+            // divi
+            // 
+            divi.Location = new Point(255, 274);
+            divi.Name = "divi";
+            divi.Size = new Size(75, 23);
+            divi.TabIndex = 15;
+            divi.Text = "%";
+            divi.UseVisualStyleBackColor = true;
+            // 
+            // punto
+            // 
+            punto.Location = new Point(93, 274);
+            punto.Name = "punto";
+            punto.Size = new Size(41, 23);
+            punto.TabIndex = 16;
+            punto.Text = ".";
+            punto.UseVisualStyleBackColor = true;
+            // 
+            // num8
+            // 
+            num8.Location = new Point(93, 74);
+            num8.Name = "num8";
+            num8.Size = new Size(75, 23);
+            num8.TabIndex = 17;
+            num8.Text = "8";
+            num8.UseVisualStyleBackColor = true;
+            num8.Click += num8_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(358, 329);
+            Controls.Add(num8);
+            Controls.Add(punto);
+            Controls.Add(divi);
+            Controls.Add(multi);
+            Controls.Add(menos);
+            Controls.Add(mas);
+            Controls.Add(num3);
+            Controls.Add(num6);
+            Controls.Add(num9);
+            Controls.Add(igual);
+            Controls.Add(num2);
+            Controls.Add(num5);
+            Controls.Add(num0);
+            Controls.Add(num1);
+            Controls.Add(num4);
+            Controls.Add(num7);
+            Controls.Add(tbDisplay);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox tbDisplay;
+        private Button num7;
+        private Button num4;
+        private Button num1;
+        private Button num0;
+        private Button num5;
+        private Button num2;
+        private Button igual;
+        private Button num9;
+        private Button num6;
+        private Button num3;
+        private Button mas;
+        private Button menos;
+        private Button multi;
+        private Button divi;
+        private Button punto;
+        private Button num8;
+    }
+}
---
+++ elpepe/Form1.cs
@@ -0,0 +1,70 @@
+namespace elpepe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void num0_Click(object sender, EventArgs e)
+        {
+            //numero 0
+            tbDisplay.Text = tbDisplay.Text + "0";
+        }
+
+        private void num1_Click(object sender, EventArgs e)
+        {
+            //numero 1
+            tbDisplay.Text = tbDisplay.Text + "1";
+        }
+
+        private void num2_Click(object sender, EventArgs e)
+        {
+            //numero 2
+            tbDisplay.Text = tbDisplay.Text + "2";
+        }
+
+        private void num3_Click(object sender, EventArgs e)
+        {
+            //numero 3
+            tbDisplay.Text = tbDisplay.Text + "3";
+        }
+
+        private void num4_Click(object sender, EventArgs e)
+        {
+            //numero 4
+            tbDisplay.Text = tbDisplay.Text + "4";
+        }
+
+        private void num5_Click(object sender, EventArgs e)
+        {
+            //numero 5
+            tbDisplay.Text = tbDisplay.Text + "5";
+        }
+
+        private void num6_Click(object sender, EventArgs e)
+        {
+            //numero 6
+            tbDisplay.Text = tbDisplay.Text + "6";
+        }
+
+        private void num7_Click(object sender, EventArgs e)
+        {
+            //numero 7
+            tbDisplay.Text = tbDisplay.Text + "7";
+        }
+
+        private void num8_Click(object sender, EventArgs e)
+        {
+            //numero 8
+            tbDisplay.Text = tbDisplay.Text + "8";
+        }
+
+        private void num9_Click(object sender, EventArgs e)
+        {
+            //numero 9
+            tbDisplay.Text = tbDisplay.Text + "9";
+        }
+    }
+}
